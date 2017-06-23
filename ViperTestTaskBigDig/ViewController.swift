ad0a3d9ecff906b83571bea79c17e9482367412b
Evgeniy Tkachenko <tkachenko-ei@MacBook-Air-Evgeniy.local>
2017-06-23 08:27:20 +0300

    Initial Commit

---
+++ ViperTestTaskBigDig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViperTestTaskBigDig
+//
+//  Created by Evgeniy Tkachenko on 23.06.17.
+//  Copyright © 2017 Big Dig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
